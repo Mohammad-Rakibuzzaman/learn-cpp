24fcb1e489cf91645dd913a47e04b5377a15d60a
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2025-03-02 13:26:24 +0600

    git new latest commit

---
+++ learn-cpp/iocp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Enter a number: ";
+    int number;
+    std::cin >> number;
+
+    std::cout << "You entered: " << number << std::endl;
+    std::cout << "Hello, World! Your terminal and compiler are working fine." << std::endl;
+
+    return 0;
+}
